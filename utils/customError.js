a27c690cc83fa5386bddcccde88a887a09e3a0cd
Castro Thomas Opiyo <copiyot@LT-COPIYOT.INDRALIMITED.local>
2020-04-12 13:57:42 +0300

    initial commit

---
+++ utils/customError.js
@@ -0,0 +1,10 @@
+function CustomError(message, code) {
+  this.name = "CustomError";
+  this.message = message;
+  this.code = code;
+}
+
+CustomError.prototype = Error;
+CustomError.constructor = CustomError;
+
+module.exports.CustomError = CustomError;
